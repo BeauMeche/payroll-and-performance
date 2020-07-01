d5703f040eff044fae6f80598dc60290e867de8d
Westley Cook <wkcook@college.harvard.edu>
2020-07-01 19:37:23 -0400

    added NHL and MLS acknowledgements

--- new-shiny/about_page_text.R
+++ new-shiny/about_page_text.R
@@ -1,30 +1,23 @@
 # this file contains text to load into the about page, in an effort to not
 # over-clutter by app.R file with text
 
-p1 <- "My project examines data from the NBA and MLB, analyzing the relationship
-between a team's payroll and their level of success in the league."
+p1 <- "My project examines data from four professional sports leagues in the
+United States (NBA, MLB, NHL, and MLS), analyzing the relationship between a 
+team's payroll  and their level of success in the league."
 
 p2 <- "Payroll is tremendously complex in professional sports because of things 
 like deferred payments (when athletes are paid for current work at future 
 dates). I simplified it here by treating it as the total amount that a team 
 spent on its current players' salaries in a given year."
 
-p3 <- '"Success" is examined here only by total wins in the regular season. It
+p3 <- '"Success" is examined here only by regular season performance. It
 would be interesting to also look at success as measured by metrics of 
 post-season performance (such as making the playoffs or winning a championship).
-My data shows that payroll correlates with regular season win percentage, but 
-will that hold true for playoff success as well? My hypothesis is that teams can
-"buy" a certain level of regular season success by paying a lot to talented 
-players, but that it does not necessarily win them a championship.'
-
-p4 <- "In the future, it would also be interesting to add analysis of NFL, NHL,
-or MLS data - but for now, I'm limited to data from the MLB and NBA because 
-their payroll data was publicly available and easy to access."
-
-p5 <- "It would also be interesting to look more closely into cases where teams 
-changed ownership/management or location, and see if that had a significant 
-impact on payroll or success, but time constraints have precluded this option 
-for now."
+My data shows that to some extent, payroll correlates with regular season win 
+percentage -- but  would that hold true for playoff success as well? My (as yet 
+untested) hypothesis is that teams can "buy" a certain level of regular season 
+success by paying a lot of money to talented players, but that doing so does not
+necessarily win them a championship.'
 
 # several chunks for the same paragraph here in order to create hyperlinks
 
@@ -39,7 +32,7 @@ p6_link_2 <- "https://www.baseball-reference.com/leagues/MLB/"
 
 p6b <- " for the data I scraped from Baseball Reference, and "
 
-p6_link_3 <- "https://github.com/westleycook/payroll-and-performance/blob/master
+scraping_code <- "https://github.com/westleycook/payroll-and-performance/blob/master
 /gather.Rmd"
 
 p6c <- " for my scraping code."
@@ -58,10 +51,45 @@ p7_link_2 <- "https://www.landofbasketball.com/nba_teams_year_by_year.htm"
 
 p7b <- " for team performance data, and "
 
-p7_link_3 <- "https://github.com/westleycook/payroll-and-performance/blob/master
-/gather.Rmd"
-
 p7c <- " for my scraping code."
+
+# several chunks for the same paragraph here as well
+
+p10 <- "My work with NHL data wouldn't have been possible without this "
+
+p10_link_1 <- "https://en.wikipedia.org/wiki/List_of_team_payrolls_in_the_NHL"
+
+p10a <- ", from which I scraped payroll data, or the folks at Hockey Reference
+who compiled "
+
+p10_link_2 <- "https://www.hockey-reference.com/teams/"
+
+p10b <- " of team performance in the NHL. For my scraping code, see "
+
+period <- "."
+
+# last acknowledgement paragraph, in chunks
+
+p11 <- "Finally, I'm indebted to the MLS Players Association, which puts out
+annual Salary Guides with salary information for all MLS players; you can find
+links to a PDF of each year's guide "
+
+p11_link_1 <- "https://mlsplayers.org/resources/salary-guide"
+
+p11a <- ". Regular season performance data for each season was compiled from 
+the "
+
+p11_link_2 <- "https://www.mlssoccer.com/standings/mls/2007/"
+
+p11b <- " and playoff results were taken from "
+
+p11_link_3 <- "https://en.wikipedia.org/wiki/2007_MLS_Cup_Playoffs"
+
+p11c <- " (although, as noted above, no analysis of the playoff data has yet
+been performed). For more detailed information on the data I used, see my 
+scraping code "
+
+# bio
 
 p8 <- "My name is Westley Cook and I'm a Harvard '20 grad with a BA in East 
 Asian Studies and a secondary in Government. You can reach me at 
@@ -69,8 +97,6 @@ westleykcook@gmail.com and find me on "
 
 p8_link <- "https://github.com/westleycook"
 
-p8a <- "."
-
-p9 <- "The repo for this project can be found "
+p9 <- "The full repo for this project can be found "
 
 p9_link <- "https://github.com/westleycook/payroll-and-performance"
